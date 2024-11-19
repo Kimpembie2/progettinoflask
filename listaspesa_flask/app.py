7cf68b1272e5956f8513258d979a0bee7993c4f9
Kimpembie2 <biello.gabriele@iisgalvanimi.edu.it>
2024-11-19 08:28:13 +0000

    Per ora funziona tutto!

--- listaspesa_flask/app.py
+++ listaspesa_flask/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+#render_template: collegare file HTML.
 #inizializza l'app Flask
 app = Flask(__name__)
 
 #rotta principale
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
+    return render_template('index.html')
 
 #avvio dell'app Flask
 if __name__ == '__main__':

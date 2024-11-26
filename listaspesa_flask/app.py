5ed0c3e435cdcc601274244588dd232b08f3aa91
Kimpembie2 <biello.gabriele@iisgalvanimi.edu.it>
2024-11-26 08:26:26 +0000

    va

--- listaspesa_flask/app.py
+++ listaspesa_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 #render_template: collegare file HTML.
 #inizializza l'app Flask
 app = Flask(__name__)
@@ -6,7 +6,20 @@ app = Flask(__name__)
 #rotta principale
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', lista=listaspesa)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        listaspesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(listaspesa):
+        listaspesa.pop(indice)
+    return redirect(url_for('home'))
 
 listaspesa = []
 #avvio dell'app Flask

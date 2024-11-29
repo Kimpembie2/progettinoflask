a9d912aedea8cf1e85ab95ec35932753dd5e0b75
Kimpembie2 <biello.gabriele@iisgalvanimi.edu.it>
2024-11-29 12:49:21 +0000

    si è cosi che si fa

--- listaspesa_flask/app.py
+++ listaspesa_flask/app.py
@@ -1,28 +1,53 @@
-from flask import Flask, render_template, request, redirect, url_for
-#render_template: collegare file HTML.
-#inizializza l'app Flask
+from flask import Flask, render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from models import db, ListaSpesa
+
 app = Flask(__name__)
 
-#rotta principale
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+lista_spese = []
+
+
 @app.route('/')
 def home():
-    return render_template('index.html', lista=listaspesa)
+    lista_spesa = ListaSpesa.query.all()
 
-@app.route('/aggiungi', methods=['POST'])
-def aggiungi():
+    return render_template('index.html' , lista_spese = lista_spesa) 
+
+@app.route('/add', methods=['POST'])
+def add():
     elemento = request.form['elemento']
     if elemento:
-        listaspesa.append(elemento)
+        nuovo_elemento = ListaSpesa(elemento=elemento)
+        db.session.add(nuovo_elemento) 
+        db.session.commit() 
+       
     return redirect(url_for('home'))
 
-@app.route('/rimuovi/<int:indice>', methods=['POST'])
-def rimuovi(indice):
-    if 0 <= indice < len(listaspesa):
-        listaspesa.pop(indice)
+@app.route('/remove/<int:indice>', methods=['POST'])
+def remove(indice):
+    elemento = ListaSpesa.query.get_or_404(indice)
+    db.session.delete(elemento)
+    db.session.commit() 
+
     return redirect(url_for('home'))
 
-listaspesa = []
-#avvio dell'app Flask
+@app.route('/delete', methods=['POST'])
+def delete():
+    ListaSpesa.query.delete() 
+    db.session.commit() 
+
+    return redirect(url_for('home'))
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
--- listaspesa_flask/models.py
+++ listaspesa_flask/models.py
@@ -1,17 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-#configurazione db
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///lista_spesa.db&';
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#inizializza SQLAlchemy
-db.init_app(app)
-#crea il database se non esiste
-with app.app_context():
-    db.create_all()
 
-#___________________________________________________________________
-#creazione tabelle
-db = SQLAlchemy() #inizialliziamo sqlAlchemy
-class ListaSpesa(db.Model): #tabella
-    id = db.Column(db.Integer, primary_key=True) #id elemento, unico  
-    elemento = db.Column(db.String(100), nullable=False) #elemento, non nullo
+db = SQLAlchemy() 
+
+class ListaSpesa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    elemento = db.Column(db.String(100), nullable=False) 
